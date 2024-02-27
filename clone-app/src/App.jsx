417a69a38a204d365ff97db4cf873fbd4ee40331
Victor <frozen711.vr.vr@gmail.com>
2024-02-26 20:42:02 -0800

    add postcss and autoprefixer in depend and also start adding header

--- clone-app/src/App.jsx
+++ clone-app/src/App.jsx
@@ -5,7 +5,25 @@ function App() {
 
   return (
     <div>
-      test
+      <header className="p-4 flex justify-between items-center">
+        <a href="" className="flexm items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+          <span className='font-bold text-xl'>Clone App</span>
+        </a>
+        <div className="flex border border-gray-300 rounded-full py-2 px-4 items-center gap-1">
+          <div>anywhere</div>
+          <div>any week</div>
+          <div>add guest</div>
+          <button>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
+
+          </button>
+        </div>
+      </header>
     </div>
   )
 }
