981233e4dd2c307ecbe52413f932ee0ab9891970
Victor <frozen711.vr.vr@gmail.com>
2024-07-24 23:46:20 -0700

    added code for pictures and function for more photos of place

--- clone-app/src/pages/PlacePage.jsx
+++ clone-app/src/pages/PlacePage.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 export default function PlacePage() {
     const { id } = useParams();
     const [place, setPlace] = useState(null);
+    const [showAllPhotos, setShowAllPhotos] = useState(false);
     useEffect(() => {
         if  (id) {
             return;
@@ -17,10 +18,77 @@ export default function PlacePage() {
         return "";
     }
 
+    if (showAllPhotos) {
+        return (
+            <div className="absolute inset-0 bg-black text-white min-h-screen">
+                <div className="bg-black p-8 grid gap-4">
+                    <div className="close">
+                        <h2 className="text-3xl">Photos of {place.title}</h2>
+                        <button className="flexed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black" onClick={() => setShowAllPhotos(false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                        </svg>
+                            Close Photos
+                        </button>
+                    </div>
+                    {place?.photos?.length > 0 && place.photos.map(photo => (
+                        <div key={photo}>
+                            <img src={`http://localhost:4000/uploads/${photo}`} alt="" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="mt-4 bg-gray-500 -mx-8 px-8 py-8">
             <h1 className="text-3xl">{place.title}</h1>
-            <a className="my-2 block font-semibold underline" target="_blank" rel="noreferrer" href={"https://maps.google.com/?q="+place.address}>{place.address}</a>
+            <a className="flex gap-1 my-2 font-semibold underline" target="_blank" rel="noreferrer" href={"https://maps.google.com/?q="+place.address}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            {place.address}
+            </a>
+            <div className="relative">
+            <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
+                <div>
+                    {place.photos?.[0] && (
+                        <div>
+                            <img
+                                className="rounded-2xl object-cover aspect-square"
+                                src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                                alt=""
+                            />
+                        </div>
+                    )}
+                </div>
+                <div className="grid grid-cols-2">
+                    {place.photos?.[1] && (
+                        <img
+                            className="rounded-2xl object-cover aspect-square"
+                            src={"http://localhost:4000/uploads/" + place.photos?.[1]}
+                            alt=""
+                        />
+                    )}
+                    <div className="overflow-hidden">
+                        {place.photos?.[2] && (
+                            <img
+                                className="rounded-2xl object-cover aspect-square relative top-2"
+                                src={"http://localhost:4000/uploads/" + place.photos?.[2]}
+                                alt=""
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+            <buttom onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow-md shadow-gray-500" >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </svg>
+                Show More Photos...</buttom>
+            </div>
         </div>
     );
 }
