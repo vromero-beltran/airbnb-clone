5606f4055f0f55604f543c5ea14f3d32b1a86a86
Victor <frozen711.vr.vr@gmail.com>
2024-07-25 01:09:21 -0700

    added date-fns and updated the checkin and out

---
+++ clone-app/src/BookingWidget.jsx
@@ -0,0 +1,54 @@
+import { differenceInCalendarDays } from 'date-fns';
+import PropTypes from 'prop-types'
+import { useState } from 'react';
+
+export function BookingWidget({place}) {
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState("");
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
+    let numberOfNights = 0;
+    if (checkIn && checkOut) {
+        numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+    }
+    return (
+        <div className="bg-white rounded-2xl p-4 shadow">
+        <div className="text-center text-2xl">
+            Price: ${place.price} / per night
+        </div>
+        <div className="border rounded-2xl mt-4">
+            <div className="flex">
+                <div className="my-3 py-4 px-4">
+                    <label>Check-in:</label>
+                    <input type="date" 
+                        value={checkIn} 
+                        onChange={ev => setCheckIn(ev.target.value)}/>
+                </div>
+                <div className="my-3 py-4 px-4 border-l">
+                    <label>Check-out</label>
+                    <input type="date"
+                        value={checkOut} 
+                        onChange={ev => setCheckOut(ev.target.value)}/>
+                </div>
+            </div>
+            <div className="my-3 py-4 px-4 border-t">
+                    <label>Number of Guests</label>
+                    <input type="number" 
+                        value={numberOfGuests} 
+                        onChange={ev => setNumberOfGuests(ev.target.value)}/>
+                </div>
+        </div>
+        <button className="primary mt-4">
+            Book this place
+            {numberOfNights > 0 && (
+                <span> ${numberOfNights * place.price}</span>
+            )}
+        </button>
+    </div>
+    )
+}
+
+BookingWidget.propTypes = {
+    place: PropTypes.shape({
+        price: PropTypes.number.isRequired,
+    }).isRequired,
+}
--- clone-app/src/pages/PlacePage.jsx
+++ clone-app/src/pages/PlacePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { BookingWidget } from "../BookingWidget";
 export default function PlacePage() {
     const { id } = useParams();
     const [place, setPlace] = useState(null);
@@ -23,7 +24,7 @@ export default function PlacePage() {
             <div className="absolute inset-0 bg-black text-white min-h-screen">
                 <div className="bg-black p-8 grid gap-4">
                     <div className="close">
-                        <h2 className="text-3xl">Photos of {place.title}</h2>
+                        <h2 className="text-3xl mr-36">Photos of {place.title}</h2>
                         <button className="flexed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black" onClick={() => setShowAllPhotos(false)}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
@@ -42,7 +43,7 @@ export default function PlacePage() {
     }
 
     return (
-        <div className="mt-4 bg-gray-500 -mx-8 px-8 py-8">
+        <div className="mt-4 bg-gray-500 -mx-8 px-8 pt-8">
             <h1 className="text-3xl">{place.title}</h1>
             <a className="flex gap-1 my-2 font-semibold underline" target="_blank" rel="noreferrer" href={"https://maps.google.com/?q="+place.address}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
@@ -57,7 +58,8 @@ export default function PlacePage() {
                     {place.photos?.[0] && (
                         <div>
                             <img
-                                className="rounded-2xl object-cover aspect-square"
+                                onClick={() => setShowAllPhotos(true)}
+                                className="rounded-2xl cursor-pointer object-cover aspect-square"
                                 src={"http://localhost:4000/uploads/" + place.photos?.[0]}
                                 alt=""
                             />
@@ -67,7 +69,8 @@ export default function PlacePage() {
                 <div className="grid grid-cols-2">
                     {place.photos?.[1] && (
                         <img
-                            className="rounded-2xl object-cover aspect-square"
+                            onClick={() => setShowAllPhotos(true)}
+                            className="rounded-2xl cursor-pointer object-cover aspect-square"
                             src={"http://localhost:4000/uploads/" + place.photos?.[1]}
                             alt=""
                         />
@@ -75,7 +78,8 @@ export default function PlacePage() {
                     <div className="overflow-hidden">
                         {place.photos?.[2] && (
                             <img
-                                className="rounded-2xl object-cover aspect-square relative top-2"
+                                onClick={() => setShowAllPhotos(true)}
+                                className="rounded-2xl cursor-pointer object-cover aspect-square relative top-2"
                                 src={"http://localhost:4000/uploads/" + place.photos?.[2]}
                                 alt=""
                             />
@@ -87,7 +91,26 @@ export default function PlacePage() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </svg>
-                Show More Photos...</buttom>
+                Show More Photos...
+                </buttom>
+            </div>
+            <div className="mt-8 mb-8 gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
+                <div>
+                    <div className="my-4">
+                        <h2 className="text-2xl font-semibold">Description</h2>
+                        {place.description}
+                    </div>
+                    Check-in: {place.checkIn} <br />
+                    Check-out: {place.checkOut} <br />
+                    Max number of Guests: {place.MaxGuests} <br />
+                </div>
+                <div>
+                    <BookingWidget place={place} />
+                </div>
+            </div>
+            <div className="bg-white -mx-8 px-8 py-8 border-t">
+                <h2 className="text-2xl font-semibold">Extra Info</h2>
+                <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
             </div>
         </div>
     );
